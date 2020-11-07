214b677fb6d172c0e05c07d455f491a179aefff8
kamillobinski <kamil.lobinski@gmail.com>
2020-11-07 13:24:10 +0100

    Add section animations

--- js/script.js
+++ js/script.js
@@ -1,144 +1,144 @@
-$(window).on("load", function(){
-    $(".loader .inner").fadeOut(500, function(){
-        $(".loader").fadeOut(800);
-    });
+$(window).on("load", function () {
+  AOS.init();
 
-    $(".items").isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
-    });
+  $(".loader .inner").fadeOut(500, function () {
+    $(".loader").fadeOut(800);
+  });
 
-})
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false,
+    },
+  });
+});
 
 //Read this function when document (page) is ready
 $(document).ready(function () {
-    //Activates superslide
-    $('#slides').superslides({
-        animation: 'fade',
-        play: 7000,
-        pagination: false
-    });
+  //Activates superslide
+  $("#slides").superslides({
+    animation: "fade",
+    play: 7000,
+    pagination: false,
+  });
 
-    //Activate typing of title elements
-    var typed = new Typed(".typed", {
-        strings: ["17 listopada 2020 r.","Światowy Tydzień Przedsiębiorczości.", "Międzynarodowy Dzień Studenta."],
-        typeSpeed: 70,
-        loop: true,
-        startDelay: 1000,
-        showCursor: false
-    });
+  //Activate typing of title elements
+  var typed = new Typed(".typed", {
+    strings: [
+      "17 listopada 2020 r.",
+      "Światowy Tydzień Przedsiębiorczości.",
+      "Międzynarodowy Dzień Studenta.",
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 
-    //Activate skills carousel 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
+  //Activate skills carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      940: {
         items: 4,
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            940: {
-                items: 4
-            }
-        }
-    })
-    //Get cursor position and start animation of loading chart
-    //where cursor reach skills section + certain delay
-    var skillsTopOffset = $(".skillsSection").offset().top;
-    var statsTopOffset = $(".statsSection").offset().top;
-    var countUpFinished = false;
+      },
+    },
+  });
+  //Get cursor position and start animation of loading chart
+  //where cursor reach skills section + certain delay
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
 
-    $(window).scroll(function () {
-        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-            $('.chart').easyPieChart({
-                easing: 'easeInOut',
-                barColor: '#fff',
-                trackColor: false,
-                scaleColor: false,
-                lineWidth: 4,
-                size: 152,
-                onStep: function (from, to, percent) {
-                    $(this.el).find('.percent').text(Math.round(percent));
-                }
-
-            });
-
-        }
-
-        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-            $(".counter").each(function () {
-                var element = $(this);
-                var endVal = parseInt(element.text());
-
-                element.countup(endVal);
-            })
-
-            countUpFinished = true;
-
-        }
-
-
-    });
-    //Activates fancy box
-    $("[data-fancybox]").fancybox();
-
-
-    $("#filters a").click(function () {
-        $("#filters .current").removeClass("current");
-        $(this).addClass("current");
-
-        var selector = $(this).attr("data-filter");
-
-        $(".items").isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 1500,
-                easing: 'linear',
-                queue: false
-            }
-        });
-
-        return false;
-    });
-
-    $("#navigation li a").click(function(e){
-        e.preventDefault();
-
-        var targetElement = $(this).attr("href");
-        var targetPosition = $(targetElement).offset().top;
-        $("html, body").animate({
-            scrollTop: targetPosition -50
-        },"slow");
-
-
-    });
-
-    const nav = $("#navigation");
-    const navTop = nav.offset().top;
-
-    $(window).on("scroll", stickyNavigation);
-
-    function stickyNavigation(){
-        var body = $("body");
-
-        if($(window).scrollTop() >= navTop){
-            body.css("padding-top", nav.outerHeight()+"px")
-            body.addClass("fixedNav");
-        }else{
-            body.css("padding-top", 0);
-            body.removeClass("fixedNav");
-        }
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
     }
 
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
 
+        element.countup(endVal);
+      });
 
+      countUpFinished = true;
+    }
+  });
+  //Activates fancy box
+  $("[data-fancybox]").fancybox();
+
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
     });
+
+    return false;
+  });
+
+  $("#navigation li a").click(function (e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: targetPosition - 50,
+      },
+      "slow"
+    );
+  });
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
+});
